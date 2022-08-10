08716cc00340fa7c436f244fef9b4253a653bcbb
duhanbektas <107396918+duhanbektas@users.noreply.github.com>
2022-08-10 17:06:31 +0300

    Update WindowTracker.js

--- useEffect-cleanupFunc/WindowTracker.js
+++ useEffect-cleanupFunc/WindowTracker.js
@@ -12,6 +12,11 @@ export default function WindowTracker() {
         
         window.addEventListener("resize", watchWidth)
         
+        /*even if our WindowTracker component is no rendered to our page, it allows us to resize our window(it's a browser component, not in our code)
+        
+        Because the window is a browser component, when the tracker is no rendered; we get an error which is called MEMORY LEAK
+        To fix this; we need to create a return func (cleanup func) in our useEffect */
+        
         return function() {
             console.log("Cleaning up...")
             window.removeEventListener("resize", watchWidth)
